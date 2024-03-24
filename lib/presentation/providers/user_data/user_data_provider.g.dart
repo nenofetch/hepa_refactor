f28c203ccc0b14984a9b10c9a5e6648c2fb3ed2d
Neno Arisma <nenoarisma@gmail.com>
2024-03-25 03:13:21 +0700

    feat(provider): add auth provider

---
+++ lib/presentation/providers/user_data/user_data_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_data_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userDataHash() => r'825b5775cff266549adf2ccaad8e7a3adde6605c';
+
+/// See also [UserData].
+@ProviderFor(UserData)
+final userDataProvider = AsyncNotifierProvider<UserData, User?>.internal(
+  UserData.new,
+  name: r'userDataProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userDataHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserData = AsyncNotifier<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
