c02471b5ae8ccc63eea08f8865c25b43fcfc300f
Neno Arisma <nenoarisma@gmail.com>
2024-04-29 17:02:01 +0700

    chore: update baseurl

--- lib/data/misc/constants.dart
+++ lib/data/misc/constants.dart
@@ -1,1 +1,1 @@
-const String baseUrl = 'http://34.68.89.81:8000/api';
+const String baseUrl = 'https://flutter.justnear.my.id/api';
--- lib/presentation/misc/constants.dart
+++ lib/presentation/misc/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const String assetsUrl = 'http://34.68.89.81:8000';
+const String assetsUrl = 'https://flutter.justnear.my.id';
 // Color
 const Color primaryColor = Color(0xff173430);
 const Color greyColor = Color(0xff617D79);
