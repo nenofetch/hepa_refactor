deae2f78aff3cf364c6faa94e65273010b297cdd
Neno Arisma <nenoarisma@gmail.com>
2024-04-29 17:02:42 +0700

    chore(page): remove untendeed char

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -5,7 +5,6 @@ import 'package:hepa/presentation/providers/user_data/user_data_provider.dart';
 import 'package:hepa/presentation/router/router_provider.dart';
 import 'package:hepa/presentation/widgets/hepa_text_field.dart';
 import 'package:flutter/material.dart';
-x
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends ConsumerWidget {
