f848a0f077a4fb05006879d71a428c376884bea3
Neno Arisma <nenoarisma@gmail.com>
2024-04-30 22:27:27 +0700

    feat: adding new hepa provider - anthropometry - reports

---
+++ lib/presentation/providers/hepa/get_reports_provider.dart
@@ -0,0 +1,30 @@
+import 'package:hepa/domain/entities/nutrition.dart';
+import 'package:hepa/domain/entities/result.dart';
+import 'package:hepa/domain/usecases/get_report/get_report.dart';
+import 'package:hepa/presentation/providers/usecases/get_report.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'get_reports_provider.g.dart';
+
+@Riverpod()
+class GetReports extends _$GetReports {
+  @override
+  FutureOr<List<Nutrition>> build() {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  Future<void> getReport() async {
+    state = const AsyncLoading();
+    GetReport getReport = ref.read(getReportProvider);
+
+    var result = await getReport(null);
+
+    switch (result) {
+      case Success(value: final reports):
+        state = AsyncData([reports]);
+      case Failed(message: _):
+        state = const AsyncData([]);
+    }
+  }
+}
