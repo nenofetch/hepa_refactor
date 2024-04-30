1d8fb2d509abbc8569eb89fcdf8b93d403219e88
Neno Arisma <nenoarisma@gmail.com>
2024-04-30 23:23:48 +0700

    refactore to getx state management! because i'm solo to developing this app

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,18 @@
+import 'package:refreshed/refreshed.dart';
+
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+    ),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hepa/firebase_options.dart';
-import 'package:hepa/presentation/misc/constants.dart';
-import 'package:hepa/presentation/router/router_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import 'package:refreshed/refreshed.dart';
+
+import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,33 +15,13 @@ void main() async {
   );
   await FirebaseMessaging.instance.setAutoInitEnabled(true);
   requestNotification();
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      theme: ThemeData.from(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: saffron,
-        ),
-        useMaterial3: true,
-        textTheme: TextTheme(
-          bodyMedium: GoogleFonts.poppins(color: blackColor),
-          bodyLarge: GoogleFonts.poppins(color: blackColor),
-          labelLarge: GoogleFonts.poppins(color: blackColor),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      routeInformationParser: ref.watch(routerProvider).routeInformationParser,
-      routerDelegate: ref.watch(routerProvider).routerDelegate,
-      routeInformationProvider:
-          ref.watch(routerProvider).routeInformationProvider,
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
 
 void requestNotification() async {
