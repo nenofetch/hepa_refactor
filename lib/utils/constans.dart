91fb83bceaedc56e6a06e9cc66874adf2f30226d
Neno Arisma <nenoarisma@gmail.com>
2024-05-01 18:01:46 +0700

    feat: add utils - constants - methods - sessions

---
+++ lib/utils/constans.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color
+const Color primaryColor = Color(0xff173430);
+const Color greyColor = Color(0xff617D79);
+const Color backgroundColor = Color(0xFf1F1D2B);
+const Color ghostWhite = Color(0xFFF6F8FF);
+const Color saffron = Color(0xFFF4C95D);
+const Color blackColor = Color(0xff072846);
+const Color neutralGrey = Color(0xffd5dae0);
+const Color blueColor = Color(0xff4157FF);
+
+// FontWeight
+const FontWeight light = FontWeight.w300;
+const FontWeight regular = FontWeight.w400;
+const FontWeight medium = FontWeight.w500;
+const FontWeight semiBold = FontWeight.w600;
+const FontWeight bold = FontWeight.bold;
+const FontWeight extraBold = FontWeight.w900;
+
+// Font Family
+TextStyle primaryFont = GoogleFonts.poppins(
+  color: primaryColor,
+);
