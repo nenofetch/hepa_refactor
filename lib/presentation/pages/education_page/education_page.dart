b49690177a10ca5ece4ab3fdcaab6fa350ca37c5
Neno Arisma <nenoarisma@gmail.com>
2024-05-06 05:29:07 +0700

    chore: remove unused code on education page

--- lib/presentation/pages/education_page/education_page.dart
+++ lib/presentation/pages/education_page/education_page.dart
@@ -14,66 +14,6 @@ class EducationPage extends ConsumerWidget {
         child: ListView(
           children: [
             ...video(educations: ref.watch(getEducationsProvider), ref: ref),
-            // ...ref.watch(getEducationsProvider).whenOrNull(
-            //           data: (data) => data.map(
-            //             (e) => Padding(
-            //               padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            //               child: Column(
-            //                 children: [
-            //                   Container(
-            //                     padding: EdgeInsets.symmetric(
-            //                       horizontal: 10,
-            //                       vertical: 10,
-            //                     ),
-            //                     decoration: BoxDecoration(
-            //                       color: Colors.blueGrey,
-            //                       borderRadius: BorderRadius.circular(12),
-            //                     ),
-            //                     child: Column(
-            //                       mainAxisSize: MainAxisSize.max,
-            //                       crossAxisAlignment: CrossAxisAlignment.start,
-            //                       children: [
-            //                         Text(
-            //                           e.title,
-            //                           style: GoogleFonts.poppins().copyWith(
-            //                             color: ghostWhite,
-            //                             fontWeight: FontWeight.bold,
-            //                             fontSize: 20,
-            //                           ),
-            //                         ),
-            //                         verticalSpace(20),
-            //                         GestureDetector(
-            //                           onTap: (Education education) => ref
-            //                               .read(routerProvider)
-            //                               .pushNamed('detail-video'),
-            //                           child: Image(
-            //                             height: 200,
-            //                             fit: BoxFit.cover,
-            //                             image: NetworkImage(
-            //                                 '$assetsUrl/storage/educations/${e.thumbnail}'),
-            //                           ),
-            //                         ),
-            //                         HtmlWidget(
-            //                           e.description,
-            //                           textStyle: TextStyle(
-            //                             color: ghostWhite,
-            //                             overflow: TextOverflow.ellipsis,
-            //                           ),
-            //                         ),
-            //                       ],
-            //                     ),
-            //                   ),
-            //                   verticalSpace(10),
-            //                 ],
-            //               ),
-            //             ),
-            //           ),
-            //         ) ??
-            //     [
-            //       Center(
-            //         child: CircularProgressIndicator(),
-            //       )
-            //     ]
           ],
         ),
       ),
