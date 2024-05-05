3c66ccfd5896be65037621e6a304e11aa8aec73f
Neno Arisma <nenoarisma@gmail.com>
2024-05-06 05:28:13 +0700

    chore: update domain layer - get logged in usecases

--- lib/domain/usecases/get_logged_in_user/get_logged_in_user.dart
+++ lib/domain/usecases/get_logged_in_user/get_logged_in_user.dart
@@ -5,26 +5,20 @@ import 'package:hepa/domain/entities/user.dart';
 import 'package:hepa/domain/usecases/usecase.dart';
 
 class GetLoggedInUser implements UseCase<Result<User>, void> {
-  final Authentication _authentication;
-  final UserRepository _userRepository;
+  final Authentication authentication;
+  final UserRepository userRepository;
 
-  GetLoggedInUser(
-      {required Authentication authentication,
-      required UserRepository userRepository})
-      : _authentication = authentication,
-        _userRepository = userRepository;
+  GetLoggedInUser({required this.authentication, required this.userRepository});
 
   @override
   Future<Result<User>> call(void _) async {
-    Result<User> token = await _authentication.getLoggedInUser();
-    if (token.isSuccess) {
-      var userResult = await _userRepository.getUser();
-
-      if (userResult.isSuccess) {
-        return Result.success(userResult.resultValue!);
-      } else {
-        return Result.failed(userResult.errorMessage!);
-      }
+    var token = await authentication.getLoggedInUser();
+    if (token is Success) {
+      var userResult = await userRepository.getUser();
+      return switch (userResult) {
+        Success(value: final user) => Result.success(user),
+        Failed(:final message) => Result.failed(message),
+      };
     } else {
       return Result.failed('User not logged in');
     }
