5f983793a4ebe0b184a0b20d4b4c10a3d6058f9e
Neno Arisma <nenoarisma@gmail.com>
2024-05-05 20:07:30 +0700

    feat: add sentry for tracking issue

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,27 @@ import 'package:hepa/firebase_options.dart';
 import 'package:hepa/presentation/misc/constants.dart';
 import 'package:hepa/presentation/router/router_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+  // Sentry for Logging
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://58f4c094c77503500459e94c8b067e4d@o314075.ingest.us.sentry.io/4507202871885824';
+      options.sampleRate = 1.0;
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const ProviderScope(child: MyApp())),
+  );
+
   requestNotification();
-  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
