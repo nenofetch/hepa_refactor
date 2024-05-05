1036816c7d4a98bd5b3b7b9b97fc0e2c68d26dbf
Neno Arisma <nenoarisma@gmail.com>
2024-05-06 05:30:42 +0700

    chore: update sport page

--- lib/presentation/pages/sport_page/sport_page.dart
+++ lib/presentation/pages/sport_page/sport_page.dart
@@ -18,77 +18,81 @@ class _SportPageState extends ConsumerState<SportPage> {
       appBar: AppBar(
         title: Text('Olahraga'),
       ),
-      body: Column(
-        children: [
-          ...ref.watch(getSportListProvider).whenOrNull(
-                    data: (sport) => sport.map(
-                      (e) => Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                  vertical: 10,
-                                ),
-                                height: 100,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.blueGrey,
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      e.name,
-                                      style: TextStyle(
-                                        color: ghostWhite,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            ...ref.watch(getSportListProvider).whenOrNull(
+                      data: (sport) => sport.map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 10,
+                                  ),
+                                  height: 100,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        e.name,
+                                        style: TextStyle(
+                                          color: ghostWhite,
+                                        ),
                                       ),
-                                    ),
-                                    verticalSpace(30),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          '${e.fiveMinutesCalories} kal/5 min',
-                                          style: TextStyle(
-                                            color: ghostWhite,
+                                      verticalSpace(30),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            '${e.fiveMinutesCalories} kal/5 min',
+                                            style: TextStyle(
+                                              color: ghostWhite,
+                                            ),
                                           ),
-                                        ),
-                                        horizontalSpace(10),
-                                        Text(
-                                          '${e.fifteenMinuteCalories} kal/15 min',
-                                          style: TextStyle(
-                                            color: ghostWhite,
+                                          horizontalSpace(10),
+                                          Text(
+                                            '${e.fifteenMinuteCalories} kal/15 min',
+                                            style: TextStyle(
+                                              color: ghostWhite,
+                                            ),
                                           ),
-                                        ),
-                                        horizontalSpace(10),
-                                        Text(
-                                          '${e.oneHourCalories} kal/1 hour',
-                                          style: TextStyle(
-                                            color: ghostWhite,
+                                          horizontalSpace(10),
+                                          Text(
+                                            '${e.oneHourCalories} kal/1 hour',
+                                            style: TextStyle(
+                                              color: ghostWhite,
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              verticalSpace(10),
-                            ],
+                                verticalSpace(10),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ) ??
-              [
-                verticalSpace(10),
-                Center(child: CircularProgressIndicator()),
-              ]
-        ],
+                    ) ??
+                [
+                  verticalSpace(10),
+                  Center(child: CircularProgressIndicator()),
+                ]
+          ],
+        ),
       ),
     );
   }
