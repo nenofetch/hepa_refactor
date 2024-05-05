972dd46f1f3f6110ce270bdef538ac361dfc6ee3
Neno Arisma <nenoarisma@gmail.com>
2024-05-06 06:17:00 +0700

    chore: change model on report repository

--- lib/data/repositories/report_repository.dart
+++ lib/data/repositories/report_repository.dart
@@ -1,11 +1,11 @@
-import 'package:hepa/domain/entities/nutrition.dart';
+import 'package:hepa/domain/entities/report.dart';
 
 import 'package:hepa/domain/entities/result.dart';
 
 abstract interface class ReportRepository {
   // Get all reports
-  Future<Result<Nutrition>> getReports();
+  Future<Result<Report>> getReports();
 
   // Add report
-  Future<Result<Nutrition>> detailReport();
+  Future<Result<Report>> detailReport();
 }
