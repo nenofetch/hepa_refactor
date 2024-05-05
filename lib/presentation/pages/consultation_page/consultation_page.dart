011a15a497c38dafb369a386fb203c38daf0d69e
Neno Arisma <nenoarisma@gmail.com>
2024-05-06 05:31:01 +0700

    chore: update consultation page

--- lib/presentation/pages/consultation_page/consultation_page.dart
+++ lib/presentation/pages/consultation_page/consultation_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class ConsultationPage extends ConsumerStatefulWidget {
   const ConsultationPage({super.key});
@@ -24,6 +26,7 @@ class _ConsultationPageState extends ConsumerState<ConsultationPage> {
       body: Chat(
         messages: _messages,
         onSendPressed: _handleSendPressed,
+        onAttachmentPressed: _handleAttachmentPressed,
         user: _user,
       ),
     );
@@ -33,6 +36,65 @@ class _ConsultationPageState extends ConsumerState<ConsultationPage> {
     setState(() {
       _messages.insert(0, message);
     });
+  }
+
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleImageSelection() async {
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: ImageSource.gallery,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: const Uuid().v4(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
   }
 
   void _handleSendPressed(types.PartialText message) {
