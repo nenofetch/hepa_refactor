0625a3006427cf4da61db8cef4813febd5ab2dce
Neno Arisma <nenoarisma@gmail.com>
2024-06-28 09:07:11 +0700

    chore(entities): update tdee entities

--- lib/domain/entities/tdee.dart
+++ lib/domain/entities/tdee.dart
@@ -2,13 +2,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'tdee.freezed.dart';
 part 'tdee.g.dart';
 
-
 @freezed
 class Tdee with _$Tdee {
- const factory Tdee({
-  required double weight,
-  required double height,
- }) = _Tdee;
-  factory Tdee.fromJson(Map<String, dynamic> json)=> _$TdeeFromJson(json);
+  const factory Tdee({
+    required String result,
+  }) = _Tdee;
+  factory Tdee.fromJson(Map<String, dynamic> json) => _$TdeeFromJson(json);
 }
-
