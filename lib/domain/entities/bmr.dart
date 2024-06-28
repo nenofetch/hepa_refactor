3c63ef2ccf637de9c05436a225479f5305f77210
Neno Arisma <nenoarisma@gmail.com>
2024-06-28 08:14:56 +0700

    feat: add bmr entities

--- lib/domain/entities/bmr.dart
+++ lib/domain/entities/bmr.dart
@@ -1,2 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'bmr.freezed.dart';
+part 'bmr.g.dart';
 
+@freezed
+class BMR with _$BMR {
+  const factory BMR({required String result}) = _BMR;
+
+  factory BMR.fromJson(Map<String, dynamic> json) => _$BMRFromJson(json);
+}
