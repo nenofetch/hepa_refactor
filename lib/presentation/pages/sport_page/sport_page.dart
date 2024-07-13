b36ef2920d053a2f52bdfb530cf87464ce35a145
Neno Arisma <nenoarisma@gmail.com>
2024-07-14 00:50:35 +0700

    chore(WIP): add submit duration of sport

--- lib/presentation/pages/sport_page/sport_page.dart
+++ lib/presentation/pages/sport_page/sport_page.dart
@@ -12,6 +12,8 @@ class SportPage extends ConsumerStatefulWidget {
 }
 
 class _SportPageState extends ConsumerState<SportPage> {
+  int _duration = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,82 +25,163 @@ class _SportPageState extends ConsumerState<SportPage> {
         child: Column(
           children: [
             ...ref.watch(getSportListProvider).whenOrNull(
-                      data: (sport) => sport.map(
-                        (e) => Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              print('clicked ${e.name}');
-                            },
-                            child: SingleChildScrollView(
-                              scrollDirection: Axis.vertical,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 10,
-                                      vertical: 10,
-                                    ),
-                                    height: 100,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blueGrey,
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          e.name,
-                                          style: TextStyle(
-                                            color: ghostWhite,
-                                            
-                                          ),
+                      data: (sport) => sport
+                          .map(
+                            (e) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return StatefulBuilder(
+                                        builder: (context, setState) {
+                                          return AlertDialog(
+                                            title: Text(
+                                                'Durasi Olahraga /15 menit'),
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Text(
+                                                    'Nama Olahraga: ${e.name}'),
+                                                verticalSpace(20),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    GestureDetector(
+                                                      onTap: () {
+                                                        setState(() {
+                                                          if (_duration > 0) {
+                                                            _duration -= 15;
+                                                          }
+                                                        });
+                                                      },
+                                                      child: Icon(Icons.remove),
+                                                    ),
+                                                    Text(
+                                                      '$_duration',
+                                                      style:
+                                                          primaryFont.copyWith(
+                                                              fontSize: 30),
+                                                    ),
+                                                    GestureDetector(
+                                                      onTap: () {
+                                                        setState(() {
+                                                          _duration += 15;
+                                                        });
+                                                      },
+                                                      child: Icon(Icons.add),
+                                                    ),
+                                                  ],
+                                                ),
+                                                verticalSpace(30),
+                                                SizedBox(
+                                                  height: 60,
+                                                  child: ElevatedButton(
+                                                    onPressed: () {},
+                                                    style: ElevatedButton
+                                                        .styleFrom(
+                                                      backgroundColor:
+                                                          blackColor,
+                                                      foregroundColor:
+                                                          backgroundColor,
+                                                      shape:
+                                                          RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(10),
+                                                      ),
+                                                    ),
+                                                    child: Text(
+                                                      'Submit',
+                                                      style: TextStyle(
+                                                        fontSize: 18,
+                                                        fontWeight: medium,
+                                                        color: ghostWhite,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      );
+                                    },
+                                  );
+                                },
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.vertical,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 10),
+                                        height: 100,
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          color: Colors.blueGrey,
+                                          borderRadius:
+                                              BorderRadius.circular(12),
                                         ),
-                                        verticalSpace(30),
-                                        Row(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
                                           children: [
                                             Text(
-                                              '${e.fiveMinutesCalories} kal/5 min',
+                                              e.name,
                                               style: TextStyle(
                                                 color: ghostWhite,
                                               ),
                                             ),
-                                            horizontalSpace(10),
-                                            Text(
-                                              '${e.fifteenMinuteCalories} kal/15 min',
-                                              style: TextStyle(
-                                                color: ghostWhite,
-                                              ),
-                                            ),
-                                            horizontalSpace(10),
-                                            Text(
-                                              '${e.oneHourCalories} kal/1 hour',
-                                              style: TextStyle(
-                                                color: ghostWhite,
-                                              ),
+                                            verticalSpace(30),
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  '${e.fiveMinutesCalories} kal/5 min',
+                                                  style: TextStyle(
+                                                    color: ghostWhite,
+                                                  ),
+                                                ),
+                                                horizontalSpace(10),
+                                                Text(
+                                                  '${e.fifteenMinuteCalories} kal/15 min',
+                                                  style: TextStyle(
+                                                    color: ghostWhite,
+                                                  ),
+                                                ),
+                                                horizontalSpace(10),
+                                                Text(
+                                                  '${e.oneHourCalories} kal/1 hour',
+                                                  style: TextStyle(
+                                                    color: ghostWhite,
+                                                  ),
+                                                ),
+                                              ],
                                             ),
                                           ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      verticalSpace(10),
+                                    ],
                                   ),
-                                  verticalSpace(10),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
+                          )
+                          .toList(),
                     ) ??
                 [
                   verticalSpace(10),
                   Center(child: CircularProgressIndicator()),
-                ]
+                ],
           ],
         ),
       ),
