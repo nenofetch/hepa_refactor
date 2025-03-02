4f26b6d289f5c9b3927346c8b80de8004ece8a21
Neno Arisma <nenoarisma@gmail.com>
2025-03-02 20:46:23 +0700

    chore: update ignore directives to include deprecated_member_use_from_same_package

--- lib/presentation/providers/user_data/user_data_provider.g.dart
+++ lib/presentation/providers/user_data/user_data_provider.g.dart
@@ -21,4 +21,4 @@ final userDataProvider = AsyncNotifierProvider<UserData, User?>.internal(
 
 typedef _$UserData = AsyncNotifier<User?>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
