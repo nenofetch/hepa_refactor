4b30c15a9ae087b617692b5bfb9070cbe66b9eeb
Neno Arisma <nenoarisma@gmail.com>
2025-06-30 11:30:07 +0700

    feat: add addEducationActivity method to EducationRepository for managing education activities

--- lib/data/api/hepa_education.dart
+++ lib/data/api/hepa_education.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:hepa/data/utils/api_url.dart';
 import 'package:hepa/data/repositories/education_repository.dart';
 import 'package:hepa/domain/entities/education.dart';
+import 'package:hepa/domain/entities/education_history.dart';
 import 'package:hepa/domain/entities/result.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -32,6 +33,29 @@ class HepaEducation implements EducationRepository {
           List<Map<String, dynamic>>.from(responses.data['data']['educations']);
 
       return Result.success(results.map((e) => Education.fromJson(e)).toList());
+    } on DioException catch (e) {
+      return Result.failed('${e.message}');
+    }
+  }
+
+  @override
+  Future<Result<EducationHistory>> addEducationActivity(
+      {required String educationName, required String tglInput}) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('token');
+
+      final response = await _dio!.post(ApiUrl.educationActivity,
+          data: {
+            'education_name': educationName,
+            'tgl_input': tglInput,
+          },
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+      if (response.statusCode == 200) {
+        return Result.success(EducationHistory.fromJson(response.data));
+      } else {
+        return Result.failed('Failed to add education activity');
+      }
     } on DioException catch (e) {
       return Result.failed('${e.message}');
     }
--- lib/data/repositories/education_repository.dart
+++ lib/data/repositories/education_repository.dart
@@ -1,7 +1,13 @@
 import 'package:hepa/domain/entities/education.dart';
+import 'package:hepa/domain/entities/education_history.dart';
 import 'package:hepa/domain/entities/result.dart';
 
 abstract interface class EducationRepository {
   // Get all educations
   Future<Result<List<Education>>> getEducations();
+
+  Future<Result<EducationHistory>> addEducationActivity({
+    required String educationName,
+    required String tglInput,
+  });
 }
